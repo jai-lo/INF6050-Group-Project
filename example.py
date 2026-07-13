21a35f01b45ff748aa72866d8c7d77411a56ee7b
jai-lo <hv7485@wayne.edu>
2026-07-13 15:42:28 -0400

    Created file with empty docstring
    Sample file for use in our assignment drafts.

---
+++ example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@Created on: 
+@Author: 
+@Course: INF 6050
+@University: Wayne State University
+@Assignment: 
+@Python Version: 3.8x
+@Required Modules: 
+@Description:
+"""
+
+
