b4253588860d242e46470af8e8bce02926845548
Lexi Flake <anflake@umich.edu>
2026-07-25 10:44:50 -0400

    function to lookup asteroid and return info

--- functions.py
+++ functions.py
@@ -1,7 +1,38 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 25 10:13:23 2026
-
-@author: anfla
+@Created on: [date here]
+@Author: [author name]
+@Course: INF 6050
+@University: Wayne State University
+@Assignment: [assignment title]
+    
+@Python Version: 3.9x   
+@Required Modules: [required modules]
+    
+@Description: [code description]
 """
+########################### 
+# IMPORT MODULES
+###########################
+import requests
 
+
+########################### 
+# GLOBAL VARIABLES
+###########################
+
+apiKey = "PMDQ9G2dHAgOeblEDAJs8cD2z4l9XyMtcMKTIjLm"
+
+########################### 
+# USER-DEFINED FUNCTIONS
+###########################
+
+def lookupAsteroid(asteroidId):
+    #instantiate dictionary to store asteroid information
+    data = {}
+    response = requests.get(f"https://api.nasa.gov/neo/rest/v1/neo/{asteroidId}?api_key={apiKey}")
+    data = response.json()
+    return data
+
+    
